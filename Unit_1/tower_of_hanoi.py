d2c18f961410603258eff9cafd1c19bf18ddea79
mdshahjadms0786-sys <mdshahjadms0786@gmail.com>
2026-03-28 19:20:43 +0530

    Clean up comments in tower_of_hanoi.py
    Removed comments and documentation from the Tower of Hanoi implementation.

--- Unit_1/tower_of_hanoi.py
+++ Unit_1/tower_of_hanoi.py
@@ -1,23 +1,11 @@
-"""
-ETCCDS202 - Data Structures
-Unit 1 Assignment
-Program 5: Tower of Hanoi
-
-Time Complexity: O(2^n)
-Space Complexity: O(n)
-Moves Formula: 2^n - 1
-"""
-
 def hanoi(n, source, helper, destination):
-    # Base Case
+
     if n == 1:
         print(f"Move disk 1 from {source} to {destination}")
         return 1   # count 1 move
     
-    # Move n-1 disks to helper
     moves1 = hanoi(n - 1, source, destination, helper)
-    
-    # Move nth disk
+
     print(f"Move disk {n} from {source} to {destination}")
     moves2 = 1
     
@@ -26,8 +14,6 @@ def hanoi(n, source, helper, destination):
     
     return moves1 + moves2 + moves3
 
-
-# -------- Main Program --------
 if __name__ == "__main__":
     try:
         num = int(input("Enter number of disks: "))
